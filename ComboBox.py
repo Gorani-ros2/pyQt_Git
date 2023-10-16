d565dafb04f177733b4bf942526ce643388c6e57
Gorani-ros2 <overtime3131@gmail.com>
2023-10-16 10:50:21 +0900

    add calenderWidget

--- ComboBox.py
+++ ComboBox.py
@@ -23,13 +23,29 @@ class WindowClass(QMainWindow, from_class):
         for day in range(1, 31+1):
             self.cbDay.addItem(str(day))
     
-        self.cbYear.setCurrentText(str(1990))  # 콤보박스 텍스트 지정
+        self.cbYear.setCurrentText(str(1990))  # 콤보박스 텍스트 지정 setCurrentText()
+        
+        self.calendarWidget.clicked.connect(self.selectDate)
+        
+    def selectDate(self):
+        date = self.calendarWidget.selectedDate()  # 캘린더 선택 selectedDate()
+        year = date.toString('yyyy') # 캘린더 텍스트 가져오기 toStirng()
+        month = date.toString('M')
+        day = date.toString('d')
+        
+        self.cbYear.setCurrentText(year)  # 콤보박스 텍스트 지정하기 setCurrentText() 
+        self.cbMonth.setCurrentText(month)
+        self.cbDay.setCurrentText(day)
+        self.lineEdit.setText(year+'-'+month.zfill(2)+'-'+day.zfill(2))
     
     def seend(self):
         year = self.cbYear.currentText()  # 콤보박스 텍스트 가져오기 currentText() 
         month = self.cbMonth.currentText()
         day = self.cbDay.currentText()
         self.lineEdit.setText(year+'-'+month.zfill(2)+'-'+day.zfill(2))
+        
+        
+        
     
 if __name__ == "__main__": 
     app = QApplication(sys.argv)
