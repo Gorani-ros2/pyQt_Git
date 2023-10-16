e950be2574f78712b15098bc2b7dc5e4c19bec4e
Gorani-ros2 <overtime3131@gmail.com>
2023-10-16 10:06:34 +0900

    add ComboBox

---
+++ ComboBox.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5 import uic
+
+
+from_class = uic.loadUiType("ComboBox.ui")[0]
+
+class WindowClass(QMainWindow, from_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle(" Test Text ComboBox ")
+        
+        self.pushButton.clicked.connect(self.seend)
+            
+        for year in range(1900,2023+1):
+            self.cbYear.addItem(str(year))  # 콤보박스 추가 addItem()
+        
+        for month in range(1,12+1):
+            self.cbMonth.addItem(str(month)) 
+            
+        for day in range(1, 31+1):
+            self.cbDay.addItem(str(day))
+    
+        self.cbYear.setCurrentText(str(1990))  # 콤보박스 텍스트 지정
+    
+    def seend(self):
+        year = self.cbYear.currentText()  # 콤보박스 텍스트 가져오기 currentText() 
+        month = self.cbMonth.currentText()
+        day = self.cbDay.currentText()
+        self.lineEdit.setText(year+'-'+month.zfill(2)+'-'+day.zfill(2))
+    
+if __name__ == "__main__": 
+    app = QApplication(sys.argv)
+    myWindows = WindowClass()
+    myWindows.show()
+    sys.exit(app.exec_())
